c5818baa01b7712e6d9b09c46989e2fbbef744ab
mike0sv <mike0sv@gmail.com>
2022-09-02 14:00:46 +0300

    init

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM python:3.9-slim
+
+RUN apt-get update && apt-get upgrade -y && apt-get install -y git && apt-get clean
+RUN pip install gto==0.2.1
+COPY entrypoint.sh .
+
+ENTRYPOINT ["entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,34 @@
+# args: ref show history
+
+set +e
+
+# TODO maybe we can skip ALL commits
+# https://stackoverflow.com/questions/10312521/how-to-fetch-all-git-branches
+git tag | xargs git tag -d
+for remote in `git branch -r`; do git branch --track ${remote#origin/} $remote; done
+git fetch --all --prune --tags
+git pull --all
+
+
+if [ "$2" = "true" ]; then
+  gto show
+fi
+
+if [ "$3" = "true" ]; then
+  gto history
+fi
+
+
+export NAME=`gto check-ref $1 --name`
+export VERSION=`gto check-ref $1 --version`
+export EVENT=`gto check-ref $GITHUB_REF --event`
+
+
+if [ "$EVENT" = "assignment" ]; then
+  export STAGE=`gto check-ref $GITHUB_REF --stage`
+fi
+
+echo "::set-output name=name::$($NAME)"
+echo "::set-output name=stage::$($STAGE)"
+echo "::set-output name=version::$($VERSION)"
+echo "::set-output name=event::$($EVENT)"
