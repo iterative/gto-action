9bf6dc6a03e02a74f35031327bf52fd6793029ca
Alexander Guschin <1aguschin@gmail.com>
2023-05-19 15:03:41 +0300

    work with nested dvc repos

--- read_annotation.py
+++ read_annotation.py
@@ -3,15 +3,28 @@ from gto.api import check_ref
 from gto.constants import SEPARATOR_IN_NAME
 from dvc import Repo
 
-artifact = check_ref(".", os.environ["GITHUB_REF"])[0].artifact
-if SEPARATOR_IN_NAME in artifact:
-    subdir, name = artifact.split(SEPARATOR_IN_NAME)
+name = check_ref(".", os.environ["GITHUB_REF"])[0].artifact
+if SEPARATOR_IN_NAME in name:
+    subdir, subname = name.split(SEPARATOR_IN_NAME)
 else:
-    subdir, name = "", artifact
+    subdir, subname = "", name
+
+path = subdir
+dvcroot = None
+while not dvcroot or path:
+    if os.path.isdir(os.path.join(path, Repo.DVC_DIR)):
+        dvcroot = path
+    path = os.path.dirname(path)
+
+if not dvcroot:
+    exit
+
+os.chdir(dvcroot)
+subdir = subdir[len(dvcroot):]
 
 r = Repo.Repo(".")
 
-annotation = r.artifacts.read().get(os.path.join(subdir, "dvc.yaml"), {}).get(name, None)
+annotation = r.artifacts.read().get(os.path.join(subdir, "dvc.yaml"), {}).get(subname, None)
 
 if annotation:
     if value := annotation.desc:
@@ -20,4 +33,3 @@ if annotation:
         os.environ["ARTIFACT_PATH"] = value
     if value := annotation.type:
         os.environ["TYPE"] = value
-
