c92ac078fa68d2ba00ca512f95ce6812493f043f
Ivan Shcheklein <shcheklein@gmail.com>
2023-07-20 20:36:43 -0700

    Add automation to bump dependencies and run basic tests (#21)
    * chore(requirements): use the requirements file to get automatic updates
    
    * chore(renovate): include forks
    
    * chore(requirements): add requirement file
    
    * chore(ci): initial CI to be able to test on PRs
    
    * chore(tests): try to use the action on this repo
    
    * chore(deps): update python docker tag to v3.11 (#1)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * chore(deps): update dependency gto to v0.3.5 (#2)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * chore(deps): update dependency dvc to v3 (#3)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * Update test.yml
    
    * fix(action): fail fast on any error in entry point
    
    * chore(tests): cleanup action, remove unused stuff
    
    * fix(action): don't fail on existing tracked branch
    
    * fix(action): don't fail of git branch of the current branch
    
    * fix(action): set all variables on all paths
    
    * chore(update): get requirements back to test the update on real repo later
    
    * fix(action): branch is not always defined when action runs
    
    * fix(action): debug with `-x`
    
    * fix(action): debug branches setup
    
    * fix(action): debug branches setup
    
    * fix(action): make branch track lax to not fail on PRs, and other strange situations
    
    * fix(action): use fetch not pull
    
    * fix(action): typo fix in error message
    
    * fix(action): update copy to be more precise
    
    * chore(deps): update requirements
    
    ---------
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,22 @@
 #!/bin/sh -l
 # args: ref show history
 
+set -eu
+
 git config --global --add safe.directory /github/workspace
 
 # TODO maybe we can skip ALL commits
 # https://stackoverflow.com/questions/10312521/how-to-fetch-all-git-branches
 git tag | xargs git tag -d
-for remote in `git branch -r`; do git branch --track ${remote#origin/} $remote; done
-git pull --all --prune --tags
+for remote in `git branch -r`; do 
+     git branch --track ${remote#origin/} $remote || \
+       echo "Failed to track `$remote` branch: already tracked or doesn't exist."
+done
+git fetch --all --prune --tags
 
 
 echo "\n\n============ GTO ============\n"
-echo "The Git tag that triggered this run: $GITHUB_REF"
+echo "The Git ref that triggered this run: $GITHUB_REF"
 
 
 export NAME=`gto check-ref $GITHUB_REF --name`
@@ -21,6 +26,8 @@ export EVENT=`gto check-ref $GITHUB_REF --event`
 
 if [ "$EVENT" = "assignment" ]; then
   export STAGE=`gto check-ref $GITHUB_REF --stage`
+else
+  export STAGE=""
 fi
 
 
@@ -28,6 +35,10 @@ if [ $NAME ]; then
   export TYPE=$(python /read_annotation.py type)
   export DESC=$(python /read_annotation.py desc)
   export ARTIFACT_PATH=$(python /read_annotation.py path)
+else
+  export TYPE=""
+  export DESC=""
+  export ARTIFACT_PATH=""
 fi
 
 
@@ -64,7 +75,7 @@ fi
 
 if [ "$5" = "true" ]; then
   if [ -z "$ARTIFACT_PATH" ]; then
-    echo "\nAnnotation is not found, can't "dvc pull" the artifact. Exiting the action."
+    echo "\nAnnotation is not found, can't `dvc pull` the artifact. Exiting the action."
     exit 1
   fi
   dvc pull $ARTIFACT_PATH || exit 1
